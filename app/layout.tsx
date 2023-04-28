03e7bbe56d00439ebea012d2754c9223acbffecb
khanglb2000 <lambaokhang2000@gmail.com>
2023-04-28 08:46:03 +0700

    [MOD] Blog navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
+import MyProfilePic from "./components/MyProfilePic";
 
 export const metadata: Metadata = {
   title: "Next Practices",
@@ -15,8 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-slate-800">
+      <body className="dark:bg-slate-800">
         <Navbar />
+        <MyProfilePic />
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,20 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={inter.className}>
-      <h1 className="text-white">Hello World!</h1>
-      <Link href="/users" className="text-white">
-        Go to Users Page
-      </Link>
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">Khang</span>.
+        </span>
+      </p>
     </main>
+
+    // <main className={inter.className}>
+    //   <h1 className="text-white">Hello World!</h1>
+    //   <Link href="/users" className="text-white">
+    //     Go to Users Page
+    //   </Link>
+    // </main>
   );
 }
